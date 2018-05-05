8355d65410935b1383e29da4183da3446dc7feb0
Christriane Marquez <christriane.marquez@gmail.com>
2018-05-05 13:22:06 -0700

    Add compression in Express Server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,20 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
+app.get('*.js', function (req, res, next) {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  res.set('Content-Type', 'text/javascript');
+  next();
+});
+
+app.get('*.css', function (req, res, next) {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  res.set('Content-Type', 'text/css');
+  next();
+});
+
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
